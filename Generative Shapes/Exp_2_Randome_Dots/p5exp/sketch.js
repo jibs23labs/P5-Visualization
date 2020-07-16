b005281f1d2cd76571fb9fcc324b94c720bf56fe
Jibs <jibs23labs@gmail.com>
2020-07-16 07:10:19 +0530

    Update sketch.js

--- Generative Shapes/Exp_2_Randome_Dots/p5exp/sketch.js
+++ Generative Shapes/Exp_2_Randome_Dots/p5exp/sketch.js
@@ -1,19 +1,33 @@
+// Initialization
 var ranx = 0;
 var rany = 0;
 var clr = 0;
+var colr = {
+        r: 0,
+        g: 0,
+        b: 0,
+};
+var x = 0;
+var y = 0;
+var w = 0;
+var h = 0;
+var b1, b2, c1, c2;
 
+// Elements and Boundaries
 function setup() {
-  // createCanvas(windowWidth,windowHeight);
-  createCanvas(1000,1000);
-
+        createCanvas(3000, 1000);
 }
 
+// Process
 function draw() {
-  ranx = random(100,900);
-  rany = random(100,900);
-  clr = random(0,255);
-  stroke(0);
-  fill(clr);
-  ellipse(ranx,rany,50,50);
-
+        ranx = random(10, 3000);
+        rany = random(10, 3000);
+        clr = random(0, 255);
+        colr.r = random(255);
+        colr.g = random(255);
+        colr.b = random(255);
+        stroke(0);
+        strokeWeight(0.1);
+        fill(colr.r, colr.g, colr.b);
+        ellipse(ranx, rany, 50, 50);
 }
